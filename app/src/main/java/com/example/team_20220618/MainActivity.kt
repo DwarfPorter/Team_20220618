e6dc6fc0120e6645a307ed4c68e4803d8bd7b78e
DwarfPorter <mrvlmor@gmail.com>
2022-06-18 21:33:34 +0300

    Кнопка

--- app/src/main/java/com/example/team_20220618/MainActivity.kt
+++ app/src/main/java/com/example/team_20220618/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.team_20220618
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
+            Toast.makeText(this, "Кнопа", Toast.LENGTH_LONG).show()
+        }
     }
 }
